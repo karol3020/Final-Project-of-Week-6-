f05b3b87269c58ceba02571f74633f7d621eedfd
Karol <K_wlodarczyk@interia.pl>
2023-02-08 00:11:34 +0100

    problems

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Logo from '../assets/logo'
+import Refresh from '@mui/icons-material/Refresh';
+import Close from '@mui/icons-material/Close';
+
 
 function Home() {
+
+    const closeContact = (event) => {
+        event.preventDefault()
+        const show = document.querySelector( '.modal__contact' , '.modal__title' 
+        , '.contact__form' , '.modal__overlay' ,
+        '.modal__overlay--loading' ,
+        'modal__overlay--success' ,
+        '.modal__overlay--loading > .MuiSvgIcon-root' ,
+         )
+        show.classList.remove( '.modal__open')
+    }
+    
+    const openContact = (event) => {
+        event.preventDefault()
+        const show = document.querySelector( '.modal__contact')
+        show.classList += ( '.modal__open')
+    }
+
+    // const sendMessage = (event) => {
+    //     event.preventDefault()
+    //     // emailjs section
+
+    //     const formpage = document.querySelector ( " .contact__form")
+    //     const loadingstage = document.querySelector(' .modal__overlay--loading')
+    //     const loadinganimation = document.querySelector( '.modal__overlay--loading > .MuiSvgIcon-root ')
+    //     const success = document.querySelector (' .modal__overlay--success')
+
+    //     setTimeout(() => {
+    //         formpage.classList += ( '.modal__negative--opacity')
+    //         loadingstage.classList += ( '.modal__show')
+    //         loadinganimation.classList += ( '.modal__show')
+    //     }, 1000);
+        
+    //     setTimeout(() => {
+    //         loadinganimation.classList += ( '.modal__hide')
+    //         success.classList += ( '.modal__show')
+    //     }, 2000);
+    // }
+
   return (
     <div className='container' >
         <nav className='navbar'>
@@ -16,9 +58,11 @@ function Home() {
 
             <div className="navbar__right">
                 <ul className='navbar__links'>
-                    <button className='color'><span>Home</span></button>
-                    <button className='color'>Find your Film</button>
-                    <button className='color'>Contact</button>
+                    <button className='pluse'>Home</button>
+                    <button className='pluse'>Find your Film</button>
+                    <button className='pluse'
+                    onClick={openContact}
+                    >Contact</button>
                 </ul>
             </div>
         </nav>
@@ -26,10 +70,12 @@ function Home() {
             <h3 className="modal__title modal__title-contact">
                 Let's have a chat
             </h3>
-            <h3 className="modal__title modal__sub-title-contact">
+            <h3 className="modal__title modal__title-contact">
                 I'm currently open to new opportunities.                
             </h3>
-            <form action="" id='contact__form'>
+            <form 
+            // onSubmit={sendMessage} 
+            className='contact__form'>
                 <div className="form__items">
                     <label className='form__item--label'>
                         Name
@@ -63,7 +109,22 @@ function Home() {
                     required
                     />
                 </div>
+                <button id='contact__submit' 
+                className='form__submit click '
+                >
+                    Send it my way 
+                </button>
             </form>
+            <div className="modal__overlay modal__overlay--loading">    
+                    <Refresh />
+            </div>
+            <div className="modal__overlay modal__overlay--success">
+              Thanks for the message !! Looking forward to speaking to you soon .
+            </div>
+            <div className="modal__exit click "> 
+                    <Close 
+                    onClick={closeContact} />
+            </div>
         </div>
     </div>
   )
